83187f135d105fff78146f05f53c6112aba15a42
jackie <joujackie@gmail.com>
2018-05-08 16:01:06 -0700

    complete board set up

--- index.js
+++ index.js
@@ -22,17 +22,32 @@ class Minesweeper {
     // 15 perceent bombs, ceil
     let cells = this.row * this.col;
     let mines = Math.ceil(cells * 0.15);
-    console.log('there are mines of', mines);
+    console.log(`there are ${mines} mines`);
     for (let i = 0; i < this.row; i++) {
       for (let j = 0; j < this.col; j++) {
         if (Math.random() < mines / cells) {
           this.board[i][j] = 9;
+          this.incAdjacent(i, j);
           mines--;
         }
         cells--;
       }
     }
-    console.log(cells, 'left');
+  }
+  incAdjacent(row, col) {
+    this.incCell(row, col - 1); // left
+    this.incCell(row, col + 1); // right
+    this.incCell(row - 1, col); // up
+    this.incCell(row + 1, col); // down
+    this.incCell(row - 1, col - 1); // up left
+    this.incCell(row + 1, col - 1); // down left
+    this.incCell(row - 1, col + 1); // up right
+    this.incCell(row + 1, col + 1); // down right
+  }
+  incCell(row, col) {
+    if (row < this.row && col < this.col && row >= 0 && col >= 0) {
+      if (this.board[row][col] !== 9) this.board[row][col]++;
+    }
   }
 }
 let test = new Minesweeper(8,6);
